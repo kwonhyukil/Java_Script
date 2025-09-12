4e7dfb55c29eef26f883681fae5ad642924c8744
Kwon_Hyuk_il <gurdlf320@naver.com>
2025-09-12 23:41:16 +0900

    javaScript study

---
+++ 1_basics/13_must_know_array_functions.js
@@ -0,0 +1,17 @@
+/**
+ * Array Functions
+ */
+let iveMembers = [
+    '안유진',
+    '가을',
+    '레이',
+    '장원영',
+    '리즈',
+    '이서',
+]
+console.log(iveMembers);
+
+// push()
+iveMembers.push('코드팩토리');
+console.log(iveMembers);
+
